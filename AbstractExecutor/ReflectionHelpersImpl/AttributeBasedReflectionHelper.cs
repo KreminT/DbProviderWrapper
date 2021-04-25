c5e9ba1947b86e8b0a7af71abd25fd760436a04b
TarasK <kremin.t@gmail.com>
2021-04-25 14:50:25 +0300

    attribute based helper

---
+++ AbstractExecutor/ReflectionHelpersImpl/AttributeBasedReflectionHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using DbProviderWrapper.Attributes;
+
+namespace DbProviderWrapper.AbstractExecutor.ReflectionHelpersImpl
+{
+    public class AttributeBasedReflectionHelper : IReflectionHelper
+    {
+        public IDictionary<string, PropertyInfo> GetProperties<TType>(TType obj)
+        {
+            Type lArgsType = typeof(TType);
+            Dictionary<string, PropertyInfo> lInfo = new Dictionary<string, PropertyInfo>();
+            foreach (PropertyInfo lPropertyInfo in lArgsType.GetProperties())
+            {
+                DbColumn lSqlArgumentAttribute = lPropertyInfo.GetCustomAttribute<DbColumn>();
+                if (lSqlArgumentAttribute != null)
+                {
+                    string lPropertyName = lSqlArgumentAttribute.Name;
+                    if (!string.IsNullOrEmpty(lPropertyName))
+                    {
+                        lInfo.Add(lPropertyName, lPropertyInfo);
+                        continue;
+                    }
+                }
+
+                lInfo.Add(lPropertyInfo.Name, lPropertyInfo);
+            }
+
+            return lInfo;
+        }
+    }
+}
---
+++ Attributes/DbColumn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DbProviderWrapper.Attributes
+{
+    public class DbColumn
+        : Attribute
+    {
+        #region Fields
+
+        private string _name;
+
+        #endregion
+
+        #region Properties
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        #endregion
+    }
+}

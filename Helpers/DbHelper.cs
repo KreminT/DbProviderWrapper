65aff54d34122ada553a7e814b783a5fec0873bd
TarasK <kremin.t@gmail.com>
2021-06-05 16:00:23 +0300

    NullIf extensions

--- Helpers/DbHelper.cs
+++ Helpers/DbHelper.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Data;
+
 #endregion
 
 namespace DbProviderWrapper.Helpers
@@ -33,5 +34,72 @@ namespace DbProviderWrapper.Helpers
             return false;
         }
 
+        public static object NullIf(this int value)
+        {
+            if (value == 0)
+                return DBNull.Value;
+            return value;
+        }
+
+        public static object NullIf(this int? value)
+        {
+            switch (value)
+            {
+                case null:
+                case 0:
+                    return DBNull.Value;
+                default:
+                    return value.Value;
+            }
+        }
+
+        public static object NullIf(this string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return DBNull.Value;
+            return value;
+        }
+
+        public static object NullIf(this double value)
+        {
+            if (value == 0)
+                return DBNull.Value;
+            return value;
+        }
+
+        public static object NullIf(double? lValue)
+        {
+            switch (lValue)
+            {
+                case null:
+                case 0:
+                    return DBNull.Value;
+                default:
+                    return lValue.Value;
+            }
+        }
+
+        public static object NullIf(this DateTime date)
+        {
+            if (date < DateTime.Now.AddYears(-20))
+                return DBNull.Value;
+            return date;
+        }
+
+        public static object NullIf(this DateTime? date)
+        {
+            if (!date.HasValue)
+                return DBNull.Value;
+            if (date.Value < DateTime.Now.AddYears(-20))
+                return DBNull.Value;
+            return date.Value;
+        }
+
+        public static object NullIf(this bool? value)
+        {
+            if (value.HasValue)
+                return value.Value;
+            return DBNull.Value;
+        }
     }
 }
